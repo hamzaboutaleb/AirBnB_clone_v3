a31716cc6127eb13d73c56b734a4edd4593ceec3
Meryeme <dobli.meryeme@gmail.com>
2023-10-01 07:49:00 -0700

    fix task6

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,17 +9,14 @@ from api.v1.views import app_views
 from models.state import State
 
 
-@app_views.route('/states', strict_slashes=False)
+@app_views.route('/states', methods=["GET"], strict_slashes=False)
 def get_states():
     """retrieves all State objects"""
-    states = storage.all(State).values()
-    states_list = []
-    for state in states:
-        states_list.append(state.to_dict())
-    return jsonify(states_list)
+    states = [state.to_dict() for state in storage.all(State).values()]
+    return jsonify(states)
 
 
-@app_views.route('/states/<state_id>', strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=["GET"], strict_slashes=False)
 def get_state_id(state_id):
     """retrieves a state object"""
     state = storage.get(State, state_id)
@@ -29,7 +26,7 @@ def get_state_id(state_id):
 
 
 @app_views.route(
-        '/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+        '/states/<state_id>', methods=["DELETE"], strict_slashes=False)
 def delete_state(state_id):
     """deletes a State object"""
     state = storage.get(State, state_id)
@@ -40,13 +37,13 @@ def delete_state(state_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
+@app_views.route('/states', methods=["POST"], strict_slashes=False)
 def create_state():
     """create a State"""
     response = request.get_json()
     if response is None:
         return jsonify({"error": "Not a JSON"}), 400
-    if 'name' not in response:
+    if "name" not in response:
         return jsonify({"error": "Missing name"}), 400
     state = State(**response)
     storage.new(state)
@@ -56,7 +53,7 @@ def create_state():
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
-    """update a State obbject"""
+    """update a State object"""
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
